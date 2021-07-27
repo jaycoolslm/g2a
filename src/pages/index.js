a096cc14f8e6eacffc5e918e26ba3af55bac8b1b
jaycoolslm <86686746+jaycoolslm@users.noreply.github.com>
2021-07-27 16:20:11 -0600

    green header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import * as React from "react"
-
+import { styles } from './styles'
 
 const IndexPage = () => {
   return (
     <main>
+      <div style={ styles.header }></div>
       <title>Home Page</title>
       
     </main>
---
+++ src/pages/styles.js
@@ -0,0 +1,11 @@
+const header = {
+    height: '10vh',
+    width: '100vw',
+    backgroundColor: 'green'
+}
+
+// styles object
+
+export const styles = {
+    header: header
+}
