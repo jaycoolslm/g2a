ae8aeb87549a0e79af3fcbb298d1c1a67b1e70e0
jaycoolslm <86686746+jaycoolslm@users.noreply.github.com>
2021-07-30 13:07:23 -0600

    added hamburger navigation

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,27 +1,60 @@
-import * as React from 'react'
+import React, { useState } from 'react';
 import { Link } from 'gatsby'
 import { StaticImage } from 'gatsby-plugin-image'
+import HamburgerMenu from '../../node_modules/react-hamburger-menu/dist/HamburgerMenu'
 
 // Stylesheet
 
 import {
   navigation,
-  logo,
-  navUl,
-  navLi
+  topPart,
+  navLi,
+  open,
+  closed,
+  logo
 } from './navbar.module.css'
 
 // Component
 
 const Navbar = () => {
+
+  const [hamburgerDisplay, hamburgerToggle] = useState(false)
+
+  const [navDisplay, navToggle] = useState(false)
+
+  function handleClick() {
+    if (!hamburgerDisplay) {
+      hamburgerToggle(true);
+      navToggle(true)
+    } else {
+      hamburgerToggle(false)
+      navToggle(false)
+    }
+  }
+
   return (
     <nav className={navigation}>
-      <StaticImage
-        alt="Get2Anywhere Logo"
-        src="../images/flatlogo.png"
-        className={logo}
-      />
-      <ul className={navUl}>
+      <div className={topPart}>
+        <StaticImage
+          alt="Get2Anywhere Logo"
+          src="../images/flatlogo.png"
+          objectFit="contain"
+          className={logo}
+        />
+        <HamburgerMenu 
+          isOpen={hamburgerDisplay}
+          menuClicked={handleClick}
+          width={50}
+          height={30}
+          strokeWidth={5}
+          rotate={0}
+          color="#5fa28f"
+          borderRadius={3}
+          animationDuration={0.5}
+        />
+      </div>
+      
+      <ul className={navDisplay ? open : closed}>
         <li><Link to="/" className={navLi}>Home</Link></li>
         <li><Link to="/about" className={navLi}>About</Link></li>
         <li><Link to="/host" className={navLi}>Host</Link></li>
