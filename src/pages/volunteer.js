958bd1954b3ccf860ccad9b6c1b79a054fd940d7
jaycoolslm <86686746+jaycoolslm@users.noreply.github.com>
2021-08-14 13:37:05 +0100

    coming soon page added

--- src/pages/volunteer.js
+++ src/pages/volunteer.js
@@ -1,10 +1,21 @@
 import * as React from 'react'
 import Navbar from '../components/navbar'
+import background from '../images/svg-assets/comingSoon.svg'
 
 const VolunteerPage = () => {
   return (
     <main>
       <Navbar></Navbar>
+      <div 
+        style={{ 
+            backgroundImage: `url(${background})`,
+            height: '90vh',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center'
+        }}>
+          <h1 style={{color: 'white', textAlign: 'center'}}>Volunteer Finder<br />Coming Soon</h1>
+      </div>
     </main>
   )
 }
